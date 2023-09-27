f8c16d4bd91fc018492d31aa96f22eb319d2847e
TuomasBorman <tvborm@utu.fi>
2023-09-27 11:16:57 +0300

    up

--- tests/testthat/test-doQuery.R
+++ tests/testthat/test-doQuery.R
@@ -2,20 +2,34 @@ context("doQuery")
 test_that("doQuery", {
     # Test that input check caches wrong arguments.
     mg <- MgnifyClient()
-    
-    expect_error(doQuery(mg, type = "studiess", accession = "MGYS00005292"))
-    expect_error(doQuery(mg, type = "studies", as.df = NULL))
-    expect_error(doQuery( mg, type = c("studies", "assembly")))
-    expect_error(doQuery(mg, type = "studies", accession = 0))
-    expect_error(doQuery(mg, type = 0))
-    expect_error(doQuery( mg, type = "studies", max.hits = 10.5 ))
-    expect_error(doQuery( mg, type = "studies", use.cache = 10))
-    expect_error(doQuery(mg, type = "studies", accession = T))
-    expect_error(doQuery(mg, type = "studies", accession = 8.5))
+
+    # Expect errors when input is wrong
+    expect_error(doQuery("test"))
+    expect_error(doQuery(TRUE))
+    expect_error(doQuery(1))
+
+    expect_error(doQuery(mg, type = 1))
+    expect_error(doQuery(mg, type = "test"))
     expect_error(doQuery(mg, type = TRUE))
-    expect_error(doQuery(mg, type = 4.9))
-    expect_error(doQuery(mg, type = "studies", accession = 3))
-    
+    expect_error(doQuery(mg, type = c("studies", "samples")))
+
+    expect_error(doQuery(mg, type = "studies", accession = 1))
+    expect_error(doQuery(mg, type = "studies", accession = TRUE))
+    expect_error(doQuery(mg, type = "studies", accession = c(1, 2)))
+
+    expect_error(doQuery(mg, type = "studies", accession = "test", as.df = NULL))
+    expect_error(doQuery(mg, type = "studies", accession = "test", as.df = 1))
+    expect_error(doQuery(mg, type = "studies", accession = "test", as.df = c(TRUE, FALSE)))
+
+    expect_error(doQuery(mg, type = "studies", accession = "test", max.hits = TRUE))
+    expect_error(doQuery(mg, type = "studies", accession = "test", max.hits = -100))
+    expect_error(doQuery(mg, type = "studies", accession = "test", max.hits = c(1, 2)))
+    expect_error(doQuery(mg, type = "studies", accession = "test", max.hits = 1.5))
+
+    expect_error(doQuery(mg, type = "studies", accession = "test", use.cache = NULL))
+    expect_error(doQuery(mg, type = "studies", accession = "test", use.cache = 1))
+    expect_error(doQuery(mg, type = "studies", accession = "test", use.cache = c(TRUE, FALSE)))
+
     # Require internet access
     skip_if(httr::http_error("https://www.ebi.ac.uk/metagenomics/api/v1"))
     # Test that studies are searched based on certain accession ID, get result
@@ -27,5 +41,6 @@ test_that("doQuery", {
     # Test that runs are searched, get result as df, choose max hits
     query2 <- doQuery(mg, "studies", "MGYS00005292", max.hits = 1)
     expect_true(is.data.frame(query2))
-    expect_equal(query2$bioproject, query$MGYS00005292$attributes$bioproject)
+    expect_equal(query2$bioproject,
+                 query$MGYS00005292$attributes$bioproject)
 })
